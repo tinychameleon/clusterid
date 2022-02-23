df25c02b181cd0f24fd1c2a8480405c7a8a62457
Stephan Tarulli <srt@tinychameleon.com>
2022-02-22 17:56:06 -0800

    Initial gem scaffolding

---
+++ lib/clusterid.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "clusterid/version"
+
+module Clusterid
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/clusterid/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Clusterid
+  VERSION = "0.1.0"
+end
