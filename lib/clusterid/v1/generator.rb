0887f86871a1578b21bae4c0d3d45e8b7cb6964f
Stephan Tarulli <srt@tinychameleon.com>
2022-02-26 12:30:38 -0800

    Fix assumption of module definitions by re-opening

--- lib/clusterid/v1/generator.rb
+++ lib/clusterid/v1/generator.rb
@@ -1,42 +1,44 @@
 # frozen_string_literal: true
 
-module ClusterId::V1
-  # Create instances of {Value} objects based on time, random data, and serializable
-  # values for data centre, environment, and type IDs.
-  class Generator
-    # @param byte_generator [#bytes(n)] an object which provides random bytes
-    # @param clock [#now_ms] an object which provides millisecond timestamps
-    # @param serializer [Serializer] an object which serializes {Value} attributes
-    # @param deserializer [Deserializer] an object which deserializes {Value} attributes
-    def initialize(byte_generator, clock, serializer, deserializer)
-      @byte_generator = byte_generator
-      @clock = clock
-      @serializer = serializer
-      @deserializer = deserializer
-    end
+module ClusterId
+  module V1
+    # Create instances of {Value} objects based on time, random data, and serializable
+    # values for data centre, environment, and type IDs.
+    class Generator
+      # @param byte_generator [#bytes(n)] an object which provides random bytes
+      # @param clock [#now_ms] an object which provides millisecond timestamps
+      # @param serializer [Serializer] an object which serializes {Value} attributes
+      # @param deserializer [Deserializer] an object which deserializes {Value} attributes
+      def initialize(byte_generator, clock, serializer, deserializer)
+        @byte_generator = byte_generator
+        @clock = clock
+        @serializer = serializer
+        @deserializer = deserializer
+      end
 
-    # Generate a {Value} for a given data centre, environment, and type ID.
-    #
-    # @param data_centre [D] a serializable value representing the data centre the {Value} is generated within
-    # @param environment [E] a serializable value representing the environment the {Value} is generated within
-    # @param type_id [T] a serializable value representing the type of entity for a generated {Value}
-    # @return [Value] a time-based, partially random value to identify an entity
-    def generate(data_centre:, environment:, type_id:)
-      raw_data = +""
-      # Nonce
-      raw_data += @byte_generator.bytes(5)
-      # Type ID
-      raw_data += [@serializer.from_type_id(type_id)].pack("S")
-      # Details (Version, Data Centre, Environment)
-      raw_data += [
-        (FORMAT_VERSION << 5) +
-        (@serializer.from_data_centre(data_centre) << 2) +
-        @serializer.from_environment(environment)
-      ].pack("C")
-      # Timestamp
-      raw_data += [@clock.now_ms].pack("Q")
+      # Generate a {Value} for a given data centre, environment, and type ID.
+      #
+      # @param data_centre [D] a serializable value representing the data centre the {Value} is generated within
+      # @param environment [E] a serializable value representing the environment the {Value} is generated within
+      # @param type_id [T] a serializable value representing the type of entity for a generated {Value}
+      # @return [Value] a time-based, partially random value to identify an entity
+      def generate(data_centre:, environment:, type_id:)
+        raw_data = +""
+        # Nonce
+        raw_data += @byte_generator.bytes(5)
+        # Type ID
+        raw_data += [@serializer.from_type_id(type_id)].pack("S")
+        # Details (Version, Data Centre, Environment)
+        raw_data += [
+          (FORMAT_VERSION << 5) +
+          (@serializer.from_data_centre(data_centre) << 2) +
+          @serializer.from_environment(environment)
+        ].pack("C")
+        # Timestamp
+        raw_data += [@clock.now_ms].pack("Q")
 
-      Value.new(raw_data.freeze, @deserializer)
+        Value.new(raw_data.freeze, @deserializer)
+      end
     end
   end
 end
--- lib/clusterid/v1/serialization.rb
+++ lib/clusterid/v1/serialization.rb
@@ -1,45 +1,47 @@
 # frozen_string_literal: true
 
-module ClusterId::V1
-  # The abstract base class of all custom serialization logic.
-  class Serializer
-    # @param t [T] the environment
-    # @return [Integer] the serialized environment
-    def from_environment(t)
-      raise NotImplementedError
+module ClusterId
+  module V1
+    # The abstract base class of all custom serialization logic.
+    class Serializer
+      # @param t [T] the environment
+      # @return [Integer] the serialized environment
+      def from_environment(t)
+        raise NotImplementedError
+      end
+
+      # @param t [T] the data centre
+      # @return [Integer] the serialized data centre
+      def from_data_centre(t)
+        raise NotImplementedError
+      end
+
+      # @param t [T] the type ID
+      # @return [Integer] the serialized type ID
+      def from_type_id(t)
+        raise NotImplementedError
+      end
     end
 
-    # @param t [T] the data centre
-    # @return [Integer] the serialized data centre
-    def from_data_centre(t)
-      raise NotImplementedError
-    end
+    # The abstract base class of all custom deserializtion logic.
+    class Deserializer
+      # @param i [Integer] the serialized environment
+      # @return [T] the deserialized environment
+      def to_environment(i)
+        raise NotImplementedError
+      end
 
-    # @param t [T] the type ID
-    # @return [Integer] the serialized type ID
-    def from_type_id(t)
-      raise NotImplementedError
-    end
-  end
+      # @param i [Integer] the serialized data centre
+      # @return [T] the deserialized data centre
+      def to_data_centre(i)
+        raise NotImplementedError
+      end
 
-  # The abstract base class of all custom deserializtion logic.
-  class Deserializer
-    # @param i [Integer] the serialized environment
-    # @return [T] the deserialized environment
-    def to_environment(i)
-      raise NotImplementedError
-    end
-
-    # @param i [Integer] the serialized data centre
-    # @return [T] the deserialized data centre
-    def to_data_centre(i)
-      raise NotImplementedError
-    end
-
-    # @param i [Integer] the serialized type ID
-    # @return [T] the deserialized type ID
-    def to_type_id(i)
-      raise NotImplementedError
+      # @param i [Integer] the serialized type ID
+      # @return [T] the deserialized type ID
+      def to_type_id(i)
+        raise NotImplementedError
+      end
     end
   end
 end
