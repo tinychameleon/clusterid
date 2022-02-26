a8a1442c29e4c4b3d7bcf5188ce56cba49e9b457
Stephan Tarulli <srt@tinychameleon.com>
2022-02-26 12:28:14 -0800

    Remove loadable intermediary V1 module

--- lib/clusterid.rb
+++ lib/clusterid.rb
@@ -3,4 +3,7 @@
 require_relative "clusterid/version"
 require_relative "clusterid/constants"
 require_relative "clusterid/errors"
-require_relative "clusterid/v1"
+
+require_relative "clusterid/v1/serialization"
+require_relative "clusterid/v1/value"
+require_relative "clusterid/v1/generator"
--- lib/clusterid/v1.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "v1/serialization"
-require_relative "v1/value"
-require_relative "v1/generator"
